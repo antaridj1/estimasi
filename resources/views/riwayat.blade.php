694c809608dace38fa3bd8af60f1c41c2dc180b8
Antaridj1 <antaridwijayanti@gmail.com>
2021-12-25 14:03:22 +0800

    riwayat blade

---
+++ resources/views/riwayat.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.main')
+
+@section('title','Riwayat | Estimasi Retribusi IMB')
+
+@section('container')
+
+<div class="container container_margin">
+    <div class="row justify-content-center">
+        <div class="col-sm-9">
+            <div class="accordion" id="accordionExample">
+                @foreach($estimasis as $estimasi)
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="heading_{{$estimasi->id}}">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_{{$estimasi->id}}" aria-expanded="true" aria-controls="collapse_{{$estimasi->id}}">
+                        {{$estimasi->total_biaya}}
+                    </button>
+                    </h2>
+                    <div id="collapse_{{$estimasi->id}}" class="accordion-collapse collapse show" aria-labelledby="heading_{{$estimasi->id}}" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <h5 class="hitung">Data Dasar</h5>
+                            <table class="table table-borderless">
+                            <tr>
+                                <td>Luas Tanah</td>
+                                <td>: {{$estimasi->luas_tanah}} m<sup>2</sup></td>
+                            </tr>
+                            <tr>
+                                <td>Luas Bangunan</td>
+                                <td>: {{$estimasi->luas_bangunan}} m<sup>2</sup></td>
+                            </tr>
+                            <tr>
+                                <td>Status Bangunan</td>
+                                <td>: {{$estimasi->gedungs->nama}}</td>
+                            </tr>
+                            </table>
+
+                            <h5 class="hitung">Sarana</h5>
+                            <table class="table table-borderless">
+                            
+                            @foreach($detail_saranas as $detail_sarana)
+                            @if($detail_sarana->estimasi_id == $estimasi->id) 
+                                <tr>
+                                    <td>{{$detail_sarana->estimasi_id->saranas}}</td>
+                                </tr>
+                            @endif
+                            @endforeach 
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
